9e8d29f45517c7c54af78d0b38a29adf5c8ace78
ayumitanaka <tanakayumi13@gmail.com>
2021-05-20 17:33:09 +0000

    backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const userRoute = require("./routes/user.route");
+
+const app = express();
+app.use(express.json());
+
+app.use("/users", userRoute);
+
+const PORT = process.env.PORT || 8080;
+mongoose.connect(
+  process.env.MONGODB_URL,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connect to Database");
+    app.listen(PORT, () =>
+      console.log(`The server has started on port: ${PORT}`)
+    );
+  }
+);
